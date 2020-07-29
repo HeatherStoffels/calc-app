ab3d40d7a5ee91827285ddd064294c14befa5e21
Heather Stoffels <Umdstarlet@gmail.com>
2020-07-29 13:10:02 -0500

    finished logic, styling

---
+++ Server/scripts/client.js
@@ -0,0 +1,87 @@
+$(document).ready(onReady);
+
+let operator = "";
+// click listeners, function that fires on page load
+function onReady() {
+  //   console.log("onReady");
+  $("#multiplyButton").on("click", multiplyFunction);
+  $("#divideButton").on("click", divideFunction);
+  $("#subtractButton").on("click", subtractFunction);
+  $("#addButton").on("click", addFunction);
+  $("#equalsButton").on("click", getResults);
+  $("#clearButton").on(
+    "click",
+    $("#numberOne").val(""),
+    $("#numberTwo").val("")
+  );
+  backFromServer();
+}
+function getResults() {
+  //   console.log("in getResults");
+  let numbersToSend = {
+    numberOne: $("#numberOne").val(),
+    numberTwo: $("#numberTwo").val(),
+    operator: operator,
+  };
+  //   console.log("attempting to post:", numbersToSend);
+  // send to server via ajax post
+
+  $.ajax({
+    type: "POST",
+    url: "/addition",
+    data: JSON.stringify(numbersToSend),
+    contentType: "application/json; charset=utf-8",
+  })
+    .then(function (response) {
+      //   console.log("back from POST", response);
+      backFromServer();
+    })
+    .catch(function (err) {
+      alert("error posting addition");
+      console.log(err);
+    });
+  //empty the inputs so new numbers can be added
+  $("#numberOne").val("");
+  $("#numberTwo").val("");
+}
+// functions that determine which operator will be sent to the server
+function addFunction() {
+  operator = "+";
+}
+function subtractFunction() {
+  operator = "-";
+}
+function multiplyFunction() {
+  operator = "*";
+}
+function divideFunction() {
+  operator = "/";
+}
+// get request back from the server with out math problem solved.
+function backFromServer() {
+  $.ajax({
+    type: "GET",
+    url: "/calculations",
+  })
+    .then(function (response) {
+      console.log("back from Get", response);
+      let el = $("#results");
+      el.empty();
+      let answer = $("#answerOut");
+
+      //loop through responses
+      for (let i = 0; i < response.length; i++) {
+        //append to DOM here
+        el.append(
+          `<li>${response[i].numberOne} ${response[i].operator} ${response[i].numberTwo} = ${response[i].result}</li>`
+        );
+        // empty answers
+        answer.empty();
+        answer.append(`${response[i].result}`);
+      }
+    })
+    .catch(function (err) {
+      alert("error posting /calculations");
+      console.log(err);
+    });
+}
--- Server/server.js
+++ Server/server.js
@@ -6,6 +6,7 @@ app.use(express.static("server/public"));
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
+
 // area for variables
 let calculatedResult = 0;
 let finishedCalculations = [];
@@ -23,6 +24,25 @@ app.listen(port, () => {
       operator: req.body.operator,
       result: calculateAnswer(),
     });
-    //   console.log(allResults)
+    
     res.send(finishedCalculations);
   });
+
+  // compute numbers function, if object.operator = "add" then push to array under answer
+function calculateAnswer() {
+    if (allResults[0].operator === "+") {
+      calculatedResult =
+        parseInt(allResults[0].numberOne) + parseInt(allResults[0].numberTwo);
+    } else if (allResults[0].operator === "-") {
+      calculatedResult =
+        parseInt(allResults[0].numberOne) - parseInt(allResults[0].numberTwo);
+    } else if (allResults[0].operator === "*") {
+      calculatedResult =
+        parseInt(allResults[0].numberOne) * parseInt(allResults[0].numberTwo);
+    } else {
+      allResults[0].operator === "/";
+      calculatedResult =
+        parseInt(allResults[0].numberOne) / parseInt(allResults[0].numberTwo);
+    }
+    return calculatedResult;
+  }
